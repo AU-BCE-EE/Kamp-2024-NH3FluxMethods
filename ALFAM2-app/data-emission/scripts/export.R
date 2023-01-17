27f13ad8de55ab2d51cf58fd65c4258bb55ee654
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-01-17 14:39:42 -0500

    Add pmid export

--- ALFAM2-app/data-emission/scripts/export.R
+++ ALFAM2-app/data-emission/scripts/export.R
@@ -2,3 +2,4 @@
 write.csv(idat, '../data/eGylle_interval.csv', row.names = FALSE)
 write.csv(pdat, '../data/eGylle_plot.csv', row.names = FALSE)
 cat(verfile, file = '../data/data_version.txt')
+write.csv(pmids, '../data/pmids.csv', row.names = FALSE)
--- ALFAM2-app/data-emission/scripts/subset.R
+++ ALFAM2-app/data-emission/scripts/subset.R
@@ -1,10 +1,12 @@
 
 pdat$app.date <- as.character(pdat$app.start, format = '%Y-%m-%d')
 
-table(pdat$app.date)
-
 #pdat <- subset(pdat, proj == 'eGylle' & app.date %in% c('2021-08-11', '2021-08-20', '2022-01-05') & meas.tech != 'DTM' & is.na(meas.tech.det))
 pdat <- subset(pdat, proj == 'eGylle')
 
 # Drop non-eGylle obs from emis interval data
 idat <- subset(idat, pmid %in% unique(pdat$pmid))
+
+# Get pmid for including in paper
+
+pmids <- aggregate(pdat$pmid, list(pdat$institute, pdat$meas.tech), FUN = function(x) paste(x, collapse = ', '))
