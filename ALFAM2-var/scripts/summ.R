d5fbc47d224f0c0570dae11b96b4e72a355a35f5
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-06-16 12:52:03 -0400

    Add meas method summary

--- ALFAM2-var/scripts/summ.R
+++ ALFAM2-var/scripts/summ.R
@@ -13,6 +13,11 @@ summ3 <- aggregate2(as.data.frame(dfinal), x = c('err1', 'err2'),
                     by = 'meas.tech2', 
                     FUN = c(mean = mean, sd = sd, n = length))
 
+# By meas tech
+summ4 <- aggregate2(dfinal, x = c('err1', 'err2'), 
+                    by = c('institute', 'meas.tech'), 
+                    FUN = c(mean = mean, sd = sd, n = length))
+
 # Counts
 counts <- dfinal[, .(n = length(e.rel), n.inst = length(unique(inst)), emis.mean = mean(e.rel), emis.sd = sd(e.rel)), by = c('app.mthd', 'meas.tech2')]
 counts <- counts[order(app.mthd, meas.tech2), ]
